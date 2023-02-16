7dbda12b0374e1f298c6b08bf0c75b875384de40
Gerencsér Bendegúz <h861280@stud.u-szeged.hu>
2023-02-16 22:14:11 +0100

    Initial commit

---
+++ src/main/java/hu/codeup/fileviewerpoc/FileviewerPocApplication.java
@@ -0,0 +1,15 @@
+package hu.codeup.fileviewerpoc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FileviewerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileviewerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/codeup/fileviewerpoc/FileviewerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.codeup.fileviewerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileviewerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
